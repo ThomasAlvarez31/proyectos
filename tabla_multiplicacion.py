25fcd3341043098ed9f2b48b8d77b98736d20eb2
ThomasAlvarez31 <163472229+ThomasAlvarez31@users.noreply.github.com>
2024-03-14 20:29:10 -0300

    Add files via upload

---
+++ tabla_multiplicacion.py
@@ -0,0 +1,16 @@
+numero = int(input('Ingrese el numero que desea obtener la tabla de multiplicacion: '))
+
+#hecho de la manera larga explicada en clases#
+
+v1=(1 * numero)
+v2=(2 * numero)
+v3=(3 * numero)
+v4=(4 * numero)
+v5=(5 * numero)
+v6=(6 * numero)
+v7=(7 * numero)
+v8=(8 * numero)
+v9=(9 * numero)
+v10=(10 * numero)
+
+print((v1),(v2),(v3),(v4),(v5),(v6),(v7),(v8),(v9),(v10))
